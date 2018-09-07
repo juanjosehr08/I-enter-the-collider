c33b176bd98e98f02cd459c7d2f328372a35d626
juanjosehr08 <juanjosehr08@gmail.com>
2018-09-07 13:50:28 -0500

    code has been modified, the user can enter more options

--- spizzle.py
+++ spizzle.py
@@ -1,18 +1,17 @@
 #spizzle v.0.1
 import random
 print("Welcome to spizzle!v.0.1")
-a = input("Insert First Name: ")
-b = input("Insert Second Name: ")
-c = input("Insert Third Name: ")
-
-
-
-def insertName(theName):
-    if theName == 1:
-        return a 
-    if theName == 2:
-        return b
-    if theName == 3:
-        return c
-print("YOUR CHOICE IS", insertName(random.randint(1,3)).upper(), "YAY!!!")
-
+print("Please insert characters then type done")
+count = 0
+i = list()
+while True:
+    name = input("Insert name:")
+    if name == "done":
+        break
+    i.append(name)
+    x = len(i)
+    ok = i[:]
+    
+#solution:
+the_solution = random.choice(ok)
+print("YOUR CHOICE IS", the_solution.upper(), "YAY!!!")
